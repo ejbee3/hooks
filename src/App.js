0cb6007723420c4277a852e356a0a3be40e1a7e6
Joe Baranski <joe.baranski49@gmail.com>
2019-06-03 16:22:14 -0400

    alpha sorta works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 import Slider from './components/Slider'
 
-class App extends Component {
-  render() {
-    return (
-      <Slider
-        randomHue={Math.ceil(Math.random() * 360)}
-        randomSaturation={Math.ceil(Math.random() * 100)}
-        randomLightness={Math.ceil(Math.random() * 100)}
-        randomAlpha={Math.ceil(Math.random() * 100)}
-      />
-    )
-  }
+export default function App() {
+  return (
+    <Slider
+      randomHue={Math.ceil(Math.random() * 360)}
+      randomSaturation={Math.ceil(Math.random() * 100)}
+      randomLightness={Math.ceil(Math.random() * 100)}
+      randomAlpha={Math.ceil(Math.random() * 100)}
+    />
+  )
 }
-
-export default App
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,19 +6,62 @@ export default function Slider(props) {
   const [lightness, setLightness] = useState(props.randomLightness)
   const [alpha, setAlpha] = useState(props.randomAlpha)
 
-
-  // const updateSlider = event => {
-  //   event.preventDefault()
-  // }
-
   return (
     <>
-     <main className="main-container">
+      <main className="main-container">
         <h1>HSL color picker</h1>
-<button onClick={() => }
-       </main>
-       </>
-   
-         
-       
- 
+        <div className="color-container">
+          <section
+            className="color-area"
+            style={{
+              backgroundColor: `hsl(${hue},${saturation}%,${lightness}%, 0.${alpha})`
+            }}
+          />
+        </div>
+
+        <div>
+          <section>
+            <p>H: {hue}</p>
+            <input
+              type="range"
+              min="1"
+              max="360"
+              value={hue}
+              onChange={e => setHue(e.target.value)}
+            />
+          </section>
+          <section>
+            <p>S: {saturation}%</p>
+            <input
+              type="range"
+              min="1"
+              max="100"
+              value={saturation}
+              onChange={e => setSaturation(e.target.value)}
+            />
+          </section>
+          <section>
+            <p>L: {lightness}%</p>
+            <input
+              type="range"
+              min="1"
+              max="100"
+              value={lightness}
+              onChange={e => setLightness(e.target.value)}
+            />
+          </section>
+          <section>
+            <p>A: {alpha}%</p>
+            <input
+              type="range"
+              min="1"
+              max="99"
+              value={alpha}
+              onChange={e => setAlpha(e.target.value)}
+            />
+          </section>
+        </div>
+      </main>
+    </>
+  )
+}
